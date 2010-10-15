acf2e901b814726bfd40d76316676e73aa0da4dc
Eric Tamme <etamme@gmail.com>
2010-10-15 15:25:40 -0400

    added some initial test data for parser work

--- digest_auth_test.rb
+++ digest_auth_test.rb
@@ -1,28 +1,34 @@
 #!/usr/bin/env ruby
 require 'digest/md5'
 
-# HA1 data
-user="test"
-realm="proxy.foo.com"
-pass="test"
+require 'testdata/registration_messages'
+puts registration.inspect
 
-# HA2 data
+# ha1 data
+user="test"
+realm="proxy.uphreak.com"
+pass="test"
+# ha2 data
 method="REGISTER"
-uri="sip:proxy.foo.com"
+uri="sip:proxy.uphreak.com"
 nonce="4cb8867c00000073a9a7c0165a3163e3a21c9065f8155260"
 
-# generate A1, HA1
-A1=[user, realm, pass].join(":")
-puts "A1 = #{A1}"
-HA1 = Digest::MD5::hexdigest(A1)
+def get_response(user,realm,pass,method,uri,nonce)
+	# generate a1, ha1
+	a1=[user, realm, pass].join(":")
+	#puts "a1 = #{a1}"
+	ha1 = Digest::MD5::hexdigest(a1)
 
-# generate A2, HA2
-A2=[method,uri].join(":")
-puts "A2 = #{A2}"
-HA2 = Digest::MD5::hexdigest(A2)
+	# generate a2, ha2
+	a2=[method,uri].join(":")
+	#puts "a2 = #{a2}"
+	ha2 = Digest::MD5::hexdigest(a2)
 
-# generate response
-request_digest=[HA1,nonce,HA2].join(":")
-puts "request_digest = #{request_digest}"
-response = Digest::MD5::hexdigest(request_digest)
-puts "response = #{response}"
+	# generate response
+	request_digest=[ha1,nonce,ha2].join(":")
+	#puts "request_digest = #{request_digest}"
+	response = Digest::MD5::hexdigest(request_digest)
+	return response
+end
+
+puts get_response(user,realm,pass,method,uri,nonce)
